bd01cf24ad7427ca9ed47ea37c65d333cc8e504e
Giulio Caprino <giulio.caprino@dataglide.co>
2024-04-08 23:16:06 +0100

    run mob next on branch with mob and changes

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import asyncio
-import subprocess
+from mob_next import mob_next
 from bleak import BleakClient
 
 BIG_BUTTON_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
-def execute_ls_command(sender: object, data: bytearray):
-    print(f"RECEIVED: {sender}: {data}")
-    result = subprocess.run(["mob", "next"], capture_output=True, text=True, cwd="/media/pmyl/Tardis/Projects")
-    print(result.stdout)
+
+def execute_mob_next(_: object, __: bytearray):
+    mob_next()
+
 
 async def main():
     address = get_address()
@@ -15,13 +15,15 @@ async def main():
     while(True):
         async with BleakClient(address) as client:
             print("Connected!")
-            await client.start_notify(BIG_BUTTON_UUID, execute_ls_command)
+            await client.start_notify(BIG_BUTTON_UUID, execute_mob_next)
             while (client.is_connected):
                 await asyncio.sleep(1)
+
 
 def get_address():
     with open("address.txt", "r") as file:
         return file.read()
+
 
 asyncio.run(main())
 
--- mob_next.py
+++ mob_next.py
@@ -2,17 +2,38 @@ import os
 import subprocess
 
 
-def mob_next(directory):
+def walk_and_run_mob_next(directory):
     for root, dirs, files in os.walk(directory):
         for name in dirs:
             path = os.path.join(root, name)
-            result = subprocess.run(["mob", "status"], capture_output=True, text=True, cwd=path)
-            print(path)
-            if "you are on wip branch mob" in result.stdout:
-                print("yes")
-            else:
-                print("no")
+            mob_status_result = run_mob_status(path)
+            if "you are on wip branch mob" in mob_status_result.stdout:
+                git_status_result = run_git_status(path)
+                if "nothing to commit, working tree clean" not in git_status_result.stdout:
+                    print(f"Found mob branch with changes in {path}")
+                    mob_next_result = run_mob_next(path)
+                    print(mob_next_result.stdout)
+                    return
 
         break
 
-mob_next("/home/vittorioguerriero/repos")
+
+def run_mob_status(path):
+    return subprocess.run(["mob", "status"], capture_output=True, text=True, cwd=path)
+
+
+def run_git_status(path):
+    return subprocess.run(["git", "status"], capture_output=True, text=True, cwd=path)
+
+
+def run_mob_next(path):
+    return subprocess.run(["mob", "next"], capture_output=True, text=True, cwd=path)
+
+
+def get_projects_path():
+    with open("projectspath.txt", "r") as file:
+        return file.read()
+
+
+def mob_next():
+    walk_and_run_mob_next(get_projects_path())
