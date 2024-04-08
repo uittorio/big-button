41e5b0293a4c4816637cdb141c2ca8aa39fdcb2c
Giulio Caprino <giulio.caprino@dataglide.co>
2024-04-08 22:36:55 +0100

    put address in file and run mob next

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import asyncio
+import subprocess
 from bleak import BleakClient
 
-address = "MAC address"
-MODEL_NBR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+BIG_BUTTON_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
-def callback(sender: object, data: bytearray):
-    print(f"{sender}: {data}")
+def execute_ls_command(sender: object, data: bytearray):
+    print(f"RECEIVED: {sender}: {data}")
+    result = subprocess.run(["mob", "next"], capture_output=True, text=True, cwd="/media/pmyl/Tardis/Projects")
+    print(result.stdout)
 
-async def main(address):
+async def main():
+    address = get_address()
+    print("Start searching...")
     while(True):
         async with BleakClient(address) as client:
-            await client.start_notify(MODEL_NBR_UUID, callback)
+            print("Connected!")
+            await client.start_notify(BIG_BUTTON_UUID, execute_ls_command)
             while (client.is_connected):
                 await asyncio.sleep(1)
 
-        
+def get_address():
+    with open("address.txt", "r") as file:
+        return file.read()
 
-asyncio.run(main(address))
+asyncio.run(main())
 
 
 
