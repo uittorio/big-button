4b41fc21d8e3ed61845876b220ca092570a510d0
Giulio Caprino <giulio.caprino@dataglide.co>
2024-05-17 08:50:33 +0100

    Add file to run mob start functionality manually

---
+++ python/start.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from config import get_config
+from mob import Mob
+
+
+async def main():
+    config = await get_config()
+    mob = Mob.create(config.projects_paths, config.verbose)
+    await mob.start()
+
+
+asyncio.run(main())
