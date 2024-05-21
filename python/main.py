d90c9e52151207114234d535300a2d06c10ebd08
Giulio Caprino <giulio.caprino@dataglide.co>
2024-05-21 11:47:18 +0100

    Automatically reconnect once disconnected

--- python/main.py
+++ python/main.py
@@ -9,8 +9,9 @@ async def main():
     config = await get_config()
 
     mob = Mob.create(config.projects_paths, config.verbose)
-    async with PicoBluetoothHandler(config.address, config.bluetooth_service_uuid, config.bluetooth_service_uuid_tx, mob) as handler:
-        await handler.keep_alive()
+    while True:
+        async with PicoBluetoothHandler(config.address, config.bluetooth_service_uuid, config.bluetooth_service_uuid_tx, mob) as handler:
+            await handler.keep_alive()
 
 
 asyncio.run(main())
